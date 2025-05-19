f5c6efb67ae3e7e9f847ff87aff3ad3e90037080
Th_Huang <84441156+Th-Huang@users.noreply.github.com>
2025-05-19 12:20:16 +0800

    Strainnet

---
+++ StrainDataset.py
@@ -0,0 +1,46 @@
+import torch
+import torch.utils.data as data_utils
+from torchvision.transforms import ToTensor
+import scipy.io as sio
+import numpy as np
+
+class MyDataset(data_utils.Dataset):
+    def __init__(self, dataset, transform=None, target_transform=None, loader=default_loader):
+        self.imgs = dataset
+        self.transform = transform
+        self.target_transform = target_transform
+        self.loader = loader
+
+    def __getitem__(self, index):
+        label_x, label_y, label_z = self.imgs[index]
+        img1 = self.loader(label_x)
+        img_1 = ToTensor()(img1.resize((128,128)))
+        img2 = self.loader(label_y)
+        img_2 = ToTensor()(img2.resize((128,128)))
+        imgs = torch.cat((img_1, img_2), 0)
+        try:
+            gt = sio.loadmat(label_z)['Disp_field_1'].astype(float)
+
+        except KeyError:
+            gt = sio.loadmat(label_z)['Disp_field_2'].astype(float)
+
+        gt = np.asarray(gt)
+        gt = gt*100
+        [dudx, dudy]= np.gradient(gt[:,:,0])
+        [dvdx, dvdy]= np.gradient(gt[:,:,1])
+
+        f = interpolate.interp2d(x, y, dudx, kind='cubic')
+        dudx_ = f(xnew, ynew)
+        f = interpolate.interp2d(x, y, dudy, kind='cubic')
+        dudy_ = f(xnew, ynew)
+        f = interpolate.interp2d(x, y, dvdx, kind='cubic')
+        dvdx_ = f(xnew, ynew)
+        f = interpolate.interp2d(x, y, dvdy, kind='cubic')
+        dvdy_ = f(xnew, ynew)
+        st = np.stack([dudx_, dudy_, dvdx_, dvdy_], axis=0)
+                #st = np.stack([dudx, dudy, dvdx, dvdy], axis=0)
+
+        return imgs,st
+
+    def __len__(self):
+        return len(self.imgs)
--- StrainNet_train.py
+++ StrainNet_train.py
@@ -1,25 +1,18 @@
 import numpy as np
+import torch
 from StrainNet import FeatureResNet, SegResNet
 from torch.autograd import Variable
-import torchvision
-
-
-
-from torchvision.transforms import ToTensor
-
+import torch.utils.data as data_utils
+import torch.nn as nn
+from  StrainDataset import MyDataset
 import io
 from torchvision import models, transforms
-import torch.utils.data as data_utils
-
 import os
-import scipy.io as sio
-from scipy import interpolate
 
+from scipy import interpolate
 import cv2
 import matplotlib.pyplot as plt
 import torch.nn.functional as F
-
-
 from torchvision.models.resnet import BasicBlock, ResNet
 from torch.nn import init
 
@@ -46,46 +39,7 @@ y = np.arange(0,256,1)
 xnew = np.arange(1.5,257.5,4)
 ynew = np.arange(1.5,257.5,4)
 
-class MyDataset(data_utils.Dataset):
-    def __init__(self, dataset, transform=None, target_transform=None, loader=default_loader):
-        self.imgs = dataset
-        self.transform = transform
-        self.target_transform = target_transform
-        self.loader = loader
 
-    def __getitem__(self, index):
-        label_x, label_y, label_z = self.imgs[index]
-        img1 = self.loader(label_x)
-        img_1 = ToTensor()(img1.resize((128,128)))
-        img2 = self.loader(label_y)
-        img_2 = ToTensor()(img2.resize((128,128)))
-        imgs = torch.cat((img_1, img_2), 0)
-        try:
-            gt = sio.loadmat(label_z)['Disp_field_1'].astype(float)
-
-        except KeyError:
-            gt = sio.loadmat(label_z)['Disp_field_2'].astype(float)
-
-        gt = np.asarray(gt)
-        gt = gt*100
-        [dudx, dudy]= np.gradient(gt[:,:,0])
-        [dvdx, dvdy]= np.gradient(gt[:,:,1])
-
-        f = interpolate.interp2d(x, y, dudx, kind='cubic')
-        dudx_ = f(xnew, ynew)
-        f = interpolate.interp2d(x, y, dudy, kind='cubic')
-        dudy_ = f(xnew, ynew)
-        f = interpolate.interp2d(x, y, dvdx, kind='cubic')
-        dvdx_ = f(xnew, ynew)
-        f = interpolate.interp2d(x, y, dvdy, kind='cubic')
-        dvdy_ = f(xnew, ynew)
-        st = np.stack([dudx_, dudy_, dvdx_, dvdy_], axis=0)
-                #st = np.stack([dudx, dudy, dvdx, dvdy], axis=0)
-
-        return imgs,st
-
-    def __len__(self):
-        return len(self.imgs)
 EPOCH = 100              # train the training data n times, to save time, we just train 100 epoch
 BATCH_SIZE = 12
 print('BATCH_SIZE = ',BATCH_SIZE)
